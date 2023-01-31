d54ce11b3ab7d8bd87d40bb0e8114c3ddba5f2a0
Andrea Bedini <andrea.bedini@tweag.io>
2023-01-31 11:50:15 +0800

    WIP

--- update.sh
+++ update.sh
@@ -2,10 +2,17 @@
 
 echo remember to do cabal update
 
-./precompute/precompute.py > bulk.jsonlines
+rm -rf data
 
-split --line-bytes 800M bulk.jsonlines bulk- --additional-suffix .jsonlines
+mkdir data
+pushd data
+
+../precompute/precompute.py > bulk.jsonlines
+
+split --line-bytes 400M bulk.jsonlines bulk- --additional-suffix .jsonlines
 
 for n in bulk-*.jsonlines; do
   jq -s < $n > $(basename $n .jsonlines).json
 done
+
+popd
